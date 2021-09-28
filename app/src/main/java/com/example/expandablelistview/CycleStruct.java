b551427c01bcd2f0231d0b91f03d2f2aaf3c0fff
Josh Zhu <Josh.Zhu@NODUS.CN>
2021-09-28 17:14:17 +0800

    cycle slide

---
+++ app/src/main/java/com/example/expandablelistview/CycleStruct.java
@@ -0,0 +1,44 @@
+package com.example.expandablelistview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CycleStruct<E> {
+
+    private List<E> list = new ArrayList<>();
+    private int index;
+    private int startIndex;
+    private boolean hasGetStarted = false;
+    public void start(int index){
+        index = index%list.size();
+        this.index = index;
+        this.startIndex = index;
+        hasGetStarted = false;
+    }
+
+    public void addData(E e){
+        list.add(e);
+    }
+
+    public E get(){
+        hasGetStarted = true;
+        E e;
+        if(index<list.size()){
+            e = list.get(index);
+        }else{
+            e = list.get(index-list.size());
+        }
+        index++;
+        return e;
+    }
+
+    public boolean canNext(){
+        if(list.size()==0){
+            return false;
+        }
+        return !hasGetStarted||
+                (index!=startIndex&&
+                        (index-list.size())!=startIndex);//没有取过数据或是数据指针没有到达开始时的下标
+    }
+}
+
--- app/src/main/java/com/example/expandablelistview/MainActivity.java
+++ app/src/main/java/com/example/expandablelistview/MainActivity.java
@@ -1,13 +1,112 @@
 package com.example.expandablelistview;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SelectorView selectorView;
+    List<String> list;
+    private TextView show;
+
+    private static final String TAG = "MainActivity";
+
+    public final ArrayList<String> unpaidList = new ArrayList<String>();
+    ArrayList<String> animalNames = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        show = (TextView) findViewById(R.id.select_year);
+
+        list = new ArrayList<>();
+
+        String testResponse = "{\n" +
+                "\t\"1111\": \"check1\",\n" +
+                "\t\"2222\": \"check2\",\n" +
+                "\t\"3333\": \"check3\",\n" +
+                "\t\"4444\": \"check4\",\n" +
+                "\t\"5555\": \"check5\",\n" +
+                "\t\"6666\": \"check6\",\n" +
+                "\t\"7777\": \"check7\",\n" +
+                "\t\"8888\": \"check8\"\n" +
+                "}";
+        JSONObject json = null;
+        try {
+            json = new JSONObject(testResponse);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        list = new ArrayList<>();
+
+        //                          JSONArray array = json.getJSONArray("Pendings");
+        Iterator<String> keys = json.keys();
+        int checkCount = 1;
+        while (keys.hasNext()) {
+            String key = keys.next();
+            String ISV = "test ISV";
+            unpaidList.add(key);
+            animalNames.add("Check "+ checkCount++);
+        }
+
+
+
+        selectorView = findViewById(R.id.selector);
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i+"月");
+        }
+
+        selectorView.setAdapter(adapter);
+
+        selectorView.setOnItemCheckListener(new SelectorView.OnItemCheckListener() {
+            @Override
+            public void onItemChecked(int position) {
+                Log.i(TAG, "onItemChecked: "+position);
+                show.setText(unpaidList.get(position));
+            }
+
+            @Override
+            public void onScrolled(int position) {
+                Log.i(TAG, "onScrolled: "+position);
+            }
+        });
     }
+
+    public void left(View view){
+        selectorView.left();
+    }
+
+    public void right(View view){
+        selectorView.right();
+    }
+
+    SelectorView.SeletcorAdapter adapter = new SelectorView.SeletcorAdapter(){
+        @Override
+        public int getItemCount() {
+            return unpaidList.size();
+        }
+
+        @Override
+        public void setView(View view, int position) {
+            ((TextView)view).setText(unpaidList.get(position));
+        }
+
+
+    };
 }
